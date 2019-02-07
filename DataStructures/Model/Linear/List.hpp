89cc0d9d684f0c8e069b0f6a849f4c0efc1418bf
Jensen Judkins <thesmelliestman@gmail.com>
2019-02-07 08:36:45 -0700

    added List.hpp and organized the data files to the resources folder

---
+++ DataStructures/Model/Linear/List.hpp
@@ -0,0 +1,33 @@
+//
+//  List.hpp
+//  DataStructures
+//
+//  Created by Judkins, Jensen on 2/7/19.
+//  Copyright © 2019 Judkins, Jensen. All rights reserved.
+//
+
+#ifndef List_hpp
+#define List_hpp
+
+#include "../Nodes/LinearNode.hpp"
+
+template <class Type>
+class List
+{
+protected:
+    int size;
+public:
+    //Structure
+    virtual void add(Type item) = 0;
+    virtual void addAtIndex(int indes, Type item) =0;
+    virtual Type remove(int index) = 0;
+    virtual Type getFromIndex(int index) = 0;
+    //Helper
+    virtual int getSize() const = 0;
+    virtual LinearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+};
+
+
+
+#endif /* List_h */
