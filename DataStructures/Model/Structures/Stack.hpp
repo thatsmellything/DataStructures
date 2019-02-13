d061ecbb888e2a65f6a0eb34ef6f2f134ef7ede1
Jensen Judkins <thesmelliestman@gmail.com>
2019-02-13 08:44:29 -0700

    Added Stack.hpp

---
+++ DataStructures/Model/Structures/Stack.hpp
@@ -0,0 +1,114 @@
+//
+//  Stack.hpp
+//  DataStructures
+//
+//  Created by Judkins, Jensen on 2/13/19.
+//  Copyright © 2019 Judkins, Jensen. All rights reserved.
+//
+
+#ifndef Stack_hpp
+#define Stack_hpp
+
+#include "LinkedList.hpp"
+
+template <class Type>
+class Stack : public LinkedList<Type>
+{
+public:
+    Stack();
+    ~Stack();
+    
+    // Stack specific methods
+    void push(Type data);
+    type pop();
+    Type peek();
+    
+    //Overriden LinkedList methods
+    void add(Type data);
+    void addAtIndex(int index, type data);
+    Type getFromIndex(int index);
+    type remove(int index);
+    
+}
+
+template <class Type>
+Stack<Type> :: Stack() : LinkedList<Type>()
+{
+    //Empty
+}
+
+template <class Type>
+Stack<Type> :: ~Stack()
+{
+    while(this->size > 0)
+    {
+        pop();
+    }
+}
+
+template <class Type>
+void Stack<Type> :: push(Type data)
+{
+    LinearNode<Type> * add = new LinearNode<type>(data);
+    
+    if(this->size == 0)
+    {
+        this->end = add;
+    }
+    else
+    {
+        add->setNextNode(this->front);
+    }
+    
+    this->front = add;
+    this->size++;
+}
+
+template <class Type>
+void Stack<type> :: add(Type data)
+{
+    push(data);
+}
+
+template <class Type>
+void Stack<type> :: addAtIndex(int index, Type data)
+{
+    assert(index == 0);
+    push(data);
+}
+
+template <class Type>
+Type Stack<Type> :: pop()
+{
+    assert (this->size > 0);
+    Type removed = this->front->getData();
+    
+    LinearNode<Type> * removedNode = this->getFront();
+    this->front = removedNode->getNextNode();
+    delete removedNode;
+    
+    this->size--;
+    return removed;
+}
+
+template <class Type>
+Type Stack<Type> :: remove(int index)
+{
+    assert(index == 0);
+    return pop();
+}
+
+template <class Type>
+Type Stack<type> :: peek()
+{
+    assert(this->size > 0);
+    return this->getFront()->getData();
+}
+
+template <class Type>
+Type Stack<Type> :: getFromIndex(int index)
+{
+    assert(index == 0);
+    return peek();
+}
+#endif /* Stack_hpp */
