d50700f55157e47a08ead2b4d86eb93cab543ce7
Jensen Judkins <thesmelliestman@gmail.com>
2019-02-21 08:01:10 -0700

    DoubleNode.hpp written

--- DataStructures/Model/Nodes/DoubleNode.hpp
+++ DataStructures/Model/Nodes/DoubleNode.hpp
@@ -6,8 +6,75 @@
 //  Copyright © 2019 Judkins, Jensen. All rights reserved.
 //
 
+#include "Node.hpp"
+
 #ifndef DoubleNode_h
 #define DoubleNode_h
+
+template <class Type>
+class DoubleNode : public Node<Type>
+{
+protected:
+    DoubleNode<Type> * previous;
+    DoubleNode<Type> * next;
+public:
+    DoubleNode();
+    DoubleNode(Type data);
+    DoubleNode(Type data, DoubleNode<Type> * previous, DoubleNode<Type> * next);
+    
+    DoubleNode<Type> * getPrevious();
+    DoubleNode<Type> * getNext();
+    
+    void setPrevious(DoubleNode<Type> * previous);
+    void setNext(DoubleNode<Type> * next);
+}
+
+template <class Type>
+DoubleNode<type> :: DoubleNode() : Node<Type>()
+{
+    previous = nullptr;
+    next = nullptr;
+}
+
+template <class Type>
+DoubleNode<Type> :: DoubleNode(Type data) : Node<Type>(data)
+{
+    previous = nullptr;
+    next = nullptr;
+}
+
+template <class Type>
+DoubleNode<Type> :: DoubleNode(Type data, DoubleNode<Type> * previous,
+                               DoubleNode<Type> * next) : Node<Type>(data)
+{
+    this->previous = previous;
+    this->next = next;
+}
+
+template <class Type>
+void DoubleNode<Type> :: setNext(DoubleNode<type> * next)
+{
+    this->next = next;
+}
+
+template <class Type>
+void DoubleNode<Type> :: setPrevious(DoubleNode<Type> * previous)
+{
+    this->previous = previous;
+}
+
+template <class Type>
+DoubleNode<Type> * DoubleNode<Type> :: getPrevious()
+{
+    return previous;
+}
+
+template <class Type>
+DouubleNode<Type> * DoubleNode<Type> :: getNext()
+{
+    return next;
+}
+
 
 
 #endif /* DoubleNode_h */
