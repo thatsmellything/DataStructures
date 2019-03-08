b420f18359ae6f95ec5a3a5538a76a89044422e2
Jensen Judkins <thesmelliestman@gmail.com>
2019-03-08 09:52:35 -0700

    graph.hpp

--- DataStructures/Model/Structures/NonLinear/Graph.hpp
+++ DataStructures/Model/Structures/NonLinear/Graph.hpp
@@ -6,8 +6,39 @@
 //  Copyright © 2019 Judkins, Jensen. All rights reserved.
 //
 
-#ifndef Graph_h
-#define Graph_h
+#ifndef Graph_hpp
+#define Graph_hpp
+#include <set>
+#include <queue>
+#include <assert.h>
 
-
-#endif /* Graph_h */
+using namespace std;
+template <class Type>
+class Graph
+{
+private:
+    static const int MAXIMUM = 50;
+    bool adjecencyMatrix [MAXIMUM][MAXIMUM];
+    int weightCostMatrix [MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirstTraversal(Graph<Type> & graph, int vertex, bool markedVertecees[]);
+    
+public:
+    Graph();
+    //Graph operations
+    
+    void addVertex(const Type& value);
+    
+    //Connect vertices
+    void addEdge(int source, int target);
+    void addEdgeUndireced(int source, int target);
+    void addEdgeCost(int source, int tareger, int cost);
+    //Disconnect vertices
+    void removeEdge(int source, int target);
+    void removeEdgeUndirected(int source, int target);
+    void removeEdgeCost(int source, int target);
+    
+    //Accessors
+    
+}
