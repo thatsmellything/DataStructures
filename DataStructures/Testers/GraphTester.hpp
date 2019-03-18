e241cc265602f83ff73e20c0e570749b2dc00729
Jensen Judkins <thesmelliestman@gmail.com>
2019-03-18 08:20:34 -0600

    added code that cody gave us

---
+++ DataStructures/Testers/GraphTester.hpp
@@ -0,0 +1,31 @@
+//
+//  GraphTester.hpp
+//  DataStructures
+//
+//  Created by Judkins, Jensen on 3/18/19.
+//  Copyright © 2019 Judkins, Jensen. All rights reserved.
+//
+
+#ifndef GraphTester_hpp
+#define GraphTester_hpp
+
+#include "../Model/NonLinear/Graph.hpp"
+#include <iostream>
+
+class GraphTester
+{
+private:
+    Graph<string> puzzle;
+    void setup();
+    void compareTraversals();
+    void findCheapestTraversal();
+    
+public:
+    void testGraphs();
+    };
+    
+    
+
+    
+
+#endif /* GraphTester_hpp */
