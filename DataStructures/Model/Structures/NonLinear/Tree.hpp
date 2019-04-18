b7dce30fb28f55afcf3b9cae9e9f47e9f5107130
Jensen Judkins <thesmelliestman@gmail.com>
2019-04-18 09:22:59 -0600

    I got t informational stubs on tree framework

--- DataStructures/Model/Structures/NonLinear/Tree.hpp
+++ DataStructures/Model/Structures/NonLinear/Tree.hpp
@@ -8,8 +8,7 @@
 
 #ifndef Tree_h
 #define Tree_h
-#include "../Nodes/BinaryTreeNode.hpp"
-
+#include "/Users/jjud0535/Documents/C++/DataStructures/DataStructures/Model/Nodes/BinaryTreeNode.hpp"
 template <class Type>
 class Tree
 {
@@ -23,13 +22,19 @@ public:
     virtual bool isBalanced() = 0;
     //Data Methods
     virtual void insert(Type data) = 0;
-    virtual bool contains(type data) = 0;
+    virtual bool contains(Type data) = 0;
     virtual remove(Type data) = 0;
     //Traversals
     virtual void inOrderTraversal() = 0;
     virtual void preOrderTraversal() = 0;
     virtual void postOrdertraversal() = 0;
 };
+//informational method stubs
+template <class Type>
+int BinarySearchTree<Type> :: getHeight()
+{
+    return -1;
+}
 
 
 #endif /* Tree_h */
